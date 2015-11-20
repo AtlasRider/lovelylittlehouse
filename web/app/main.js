fe2a38b778ba0c7e16c63c25800dcaf90fbab5b0
AtlasRider <dwyer.bill@gmail.com>
2015-11-19 19:42:55 -0800

    Splitting into separate files

--- web/app/main.js
+++ web/app/main.js
@@ -1,24 +1,2 @@
 var app = angular.module("lovelyapp", []);
 
-
-app.directive('lovelyheader', function() {
-	return {
-		restrict: 'E',
-		templateUrl: 'app/shared/header/header.html',
-		controller: 'HeaderController'
-	};
-
-});
-
-app.controller('HeaderController', [HeaderController]);
-
-function HeaderController() {
-	
-	skrollr.init({
-        render: function(data) {            	
-    	// $('#offset').text(data.curTop);  //Log the current scroll position.
-    	},
-    	forceHeight:false
-    });
-
-}
--- web/app/shared/header/header.js
+++ web/app/shared/header/header.js
@@ -1,3 +1,20 @@
-app.controller("lovelyHeader", function(scope) {
+
+app.directive('lovelyheader', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'app/shared/header/header.html',
+		controller: 'HeaderController'
+	};
 
 });
+
+app.controller('HeaderController', [function (){
+	
+	skrollr.init({
+	            render: function(data) {            	
+	            	// $('#offset').text(data.curTop);  //Log the current scroll position.
+	            	},
+	            	forceHeight:false
+	            });
+
+}]);
